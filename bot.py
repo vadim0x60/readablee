2219dfee701378aeaaa11b13d8937b61f3cc77a3
Vadim Liventsev <liventsevadim@gmail.com>
2018-11-25 04:44:19 +0300

    IndentationError

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import uuid
 from flask import Flask, request
 import io
 import os
+import scipy
 
 from google.cloud import speech
 from google.cloud.speech import enums
@@ -13,20 +14,22 @@ from google.cloud.speech import types
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'iam.json'
 client = speech.SpeechClient()
 
-def download(url, filename='a.ogg'):
+def fetch_wav(filename='a.wav'):
     os.system(f'wget -O {filename} {url}')
-
-audio_config = types.RecognitionConfig(
-    encoding=enums.RecognitionConfig.AudioEncoding.OGG_OPUS,
-    sample_rate_hertz=16000,
-    language_code='ru-RU')
-
-def transcribe(filename='a.ogg'):
-    # Loads the audio into memory
-    with io.open(filename, 'rb') as audio_file:
-        content = audio_file.read()
-        audio = types.RecognitionAudio(content=content)
+    ogg_filename = path2ogg.replace('wav', 'ogg')
+    os.system('ffmpeg -i {0} {1}'.format(ogg_filename, filename))
     
+def transcribe(wavpath='a.wav'):
+    # Loads the audio into memory
+	sample_rate, samples = scipy.io.wavfile.read(wavpath)
+	print(samples.mean(axis=1))
+	audio = types.RecognitionAudio(content=samples.mean(axis=1))
+	
+    audio_config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=sample_rate,
+        language_code='ru-RU')
+        
     # Detects speech in the audio file
     response = client.recognize(audio_config, audio)
     print(response)
@@ -43,7 +46,7 @@ def handle_msg(message):
     if 'attachments' in message:
         for attachment in message['attachments']:
             if attachment['type'] == 'audio_message':
-                download(attachment['audio_message']['link_ogg'])
+                fetch_wav(attachment['audio_message']['link_ogg'])
                 print(vk('messages.send', user_id=message['from_id'], 
                                           message=transcribe(), 
                                           random_id=uuid.uuid4().int))
